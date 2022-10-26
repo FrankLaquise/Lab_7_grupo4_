038822d905a90a2c0effefce82b0e7683ee6b4fa
elias <m.yovera@pucp.edu.pe>
2022-10-26 15:34:49 -0500

    SolicitudesController

---
+++ src/main/java/com/example/lab_7_grupo4_/controller/SolicitudesController.java
@@ -0,0 +1,18 @@
+package com.example.lab_7_grupo4_.controller;
+
+import com.example.lab_7_grupo4_.entity.Usuario;
+import com.example.lab_7_grupo4_.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class SolicitudesController {
+    @Autowired
+    UserRepository userRepository;
+
+
+}
