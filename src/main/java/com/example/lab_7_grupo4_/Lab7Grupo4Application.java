d039303aeea268c0f235e58916d2aa5551db6b0c
FrankLaquise <a20175947@pucp.edu.pe>
2022-10-26 10:58:55 -0500

    Initial commit

---
+++ src/main/java/com/example/lab_7_grupo4_/Lab7Grupo4Application.java
@@ -0,0 +1,13 @@
+package com.example.lab_7_grupo4_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7Grupo4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7Grupo4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab_7_grupo4_/Lab7Grupo4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_7_grupo4_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7Grupo4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
