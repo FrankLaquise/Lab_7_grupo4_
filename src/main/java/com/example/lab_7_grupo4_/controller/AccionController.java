27ccdd3bb1c8b3ab1e81e0d371b45699c0208adb
FrankLaquise <a20175947@pucp.edu.pe>
2022-10-26 23:55:34 -0500

    pregunta1

--- src/main/java/com/example/lab_7_grupo4_/controller/AccionController.java
+++ src/main/java/com/example/lab_7_grupo4_/controller/AccionController.java
@@ -5,6 +5,7 @@ import com.example.lab_7_grupo4_.repository.AccioneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 
 @RestController
+@CrossOrigin
 public class AccionController {
 
     @Autowired
--- src/main/java/com/example/lab_7_grupo4_/controller/SolicitudesController.java
+++ src/main/java/com/example/lab_7_grupo4_/controller/SolicitudesController.java
@@ -15,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
+@CrossOrigin
 public class SolicitudesController {
     @Autowired
     SolicitudesRepository solicitudesRepository;
